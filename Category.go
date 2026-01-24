a414eb4490466a3953bcda60931f3f775364155a
Ali <ali2007kazybai@gmail.com>
2026-01-24 15:45:19 +0500

    Category and Spare parts

---
+++ Category.go
@@ -0,0 +1,12 @@
+package awesomeProject1
+
+type Categories struct {
+	ID          string   `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	PartsList   []string `json:"parts_list"`
+}
+
+func (cat *Categories) AddCategory()        {}
+func (cat *Categories) ListParts() []string { return cat.PartsList }
+func (cat *Categories) UpdateCategory()     {}
